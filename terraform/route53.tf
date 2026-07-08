4fcd55aad0495bd5b28771c807cf0327e2e2710e
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2026-07-08 16:06:53 +0300

    final test 1

---
+++ terraform/route53.tf
@@ -0,0 +1,24 @@
+resource "aws_route53_zone" "private_zone" {
+  name = "vpro.local" 
+
+  vpc {
+    vpc_id = module.vpc.vpc_id 
+  }
+}
+
+resource "aws_route53_record" "rabbitmq_record" {
+  zone_id = aws_route53_zone.private_zone.zone_id
+  name    = "rmq01.vpro.local"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.rabbitmq_server.private_ip]
+}
+
+resource "aws_route53_record" "memcached_record" {
+  zone_id = aws_route53_zone.private_zone.zone_id
+  name    = "mc01.vpro.local"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.memcached_server.private_ip]
+}
+
