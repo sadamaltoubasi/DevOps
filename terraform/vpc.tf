760c804e5f9932a009d803e6ce1885420eabf9dc
Sadam Altoubasi <sadam.altoubasi@yahoo.com>
2026-07-27 15:08:50 +0300

    test

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,9 +8,9 @@ module "vpc" {
   private_subnets = [var.PrivateSubnet1, var.PrivateSubnet2, var.PrivateSubnet3]
   public_subnets  = [var.PublicSubnet1, var.PublicSubnet2, var.PublicSubnet3]
 
-  enable_nat_gateway      = true
-  # one_nat_gateway_per_az  = true
-  single_nat_gateway      = true
+  enable_nat_gateway     = true
+  one_nat_gateway_per_az = true
+  #single_nat_gateway      = true
   enable_dns_hostnames    = true
   enable_dns_support      = true
   map_public_ip_on_launch = true
